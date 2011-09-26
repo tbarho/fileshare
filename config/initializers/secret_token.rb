6fae84918c336892441ad74be0e7c890a80cca12
Ty Barho <t.barho@gmail.com>
2011-09-26 18:47:58 +0000

    first commit, added rspec folders, ran rails g rspec:install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fileshare::Application.config.secret_token = 'b16617c8bc2f1ecfaf40ba0be847b8cc1a5bbda64b17b27d06c21e7ee3317ba01956021a743934f8e5ab2a1ffa06e3cafd05026169f104e8ee8c3604b2d4fb77'
